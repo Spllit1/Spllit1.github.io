2f5376e17aebdc80a3b60df1838b922e0f3d8080
Spllit1 <106594185+Spllit1@users.noreply.github.com>
2022-08-31 09:46:18 +0200

    Uploading the website test
    hm

---
+++ index.js
@@ -0,0 +1,154 @@
+
+console.log("Working :)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let x= Math.floor(Math.random() * 6 +1)
+
+console.log(x);
+
+document.getElementById("randbutt").onclick = function(){
+    x= Math.floor(Math.random() * 6 +1)
+
+    document.getElementById("randnum").innerHTML = x;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let count = 0;
+
+document.getElementById("decrease").onclick = function(){
+  count-=1
+  document.getElementById("countlabel").innerHTML = count;
+}
+
+document.getElementById("reset").onclick = function(){
+  count=0
+  document.getElementById("countlabel").innerHTML = count;
+}
+
+document.getElementById("increase").onclick = function(){
+  count+=1
+  document.getElementById("countlabel").innerHTML = count;
+}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let a = window.prompt("1. Zahl zum addieren")
+let b = window.prompt("2. Zahl zum addieren")
+
+a= Number(a);
+b= Number(b);
+
+
+window.prompt("1. Zahl + 2. Zahl = " + Number(a+b) + ".", a+b)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let age = window.prompt("How old are u?")
+
+console.log(typeof age)
+age = Number(age);
+age += 1
+console.log(typeof age)
+console.log("Happy birthday! You are ", age, " years old!")
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let username;
+document.getElementById("myButton").onclick = function(){
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("mylabel").innerHTML = "Hello, "+username+"!";
+}*/
