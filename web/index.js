3d264059613e3e5b69e847a38ed303f53880ae8e
Spllit1 <106594185+Spllit1@users.noreply.github.com>
2022-09-05 17:24:47 +0200

    Big Update!
    Maded the Mian page smoother/added 2 options to it!
    The 404 Page also got smoother!

--- web/index.js
+++ web/index.js
@@ -1,2 +1,10 @@
-console.log("Server is up!")
+function g1(){
+    window.location.replace("http://spllit1.github.io/web/snake");
+}
+
+function g2(){
+    window.location.replace("http://spllit1.github.io/web/ThisIsNotAnAviablePage");
+}
+
+
 
